a49b0e874ac7d698c868ee98b5537cfd476a66b5
devnote-dev <devnote.dev75@gmail.com>
2023-08-16 23:40:24 +0100

    chore(env): code cleanup for initialization

--- src/env.cr
+++ src/env.cr
@@ -1,31 +1,27 @@
 module Crimson::ENV
-  CRIMSON_LIBRARY = begin
-    {% if flag?(:win32) %}
-      Path[::ENV["APPDATA"]] / "crimson"
-    {% else %}
-      if data = ::ENV["XDG_DATA_HOME"]?
-        Path[data] / "crimson"
-      else
-        Path.home / ".local" / "share" / "crimson"
-      end
-    {% end %}
-  end
+  CRIMSON_LIBRARY = {% if flag?(:win32) %}
+                      Path[::ENV["APPDATA"], "crimson"]
+                    {% else %}
+                      begin
+                        if data = ::ENV["XDG_DATA_HOME"]?
+                          Path[data, "crimson"]
+                        else
+                          Path.home / ".local" / "share" / "crimson"
+                        end
+                      end
+                    {% end %}
 
-  CRYSTAL_CACHE = begin
-    {% if flag?(:win32) %}
-      Path[::ENV["LOCALAPPDATA"]] / "crystal"
-    {% else %}
-      Path.home / ".cache" / "crystal"
-    {% end %}
-  end
+  CRYSTAL_CACHE = {% if flag?(:win32) %}
+                    Path[::ENV["LOCALAPPDATA"], "crystal"]
+                  {% else %}
+                    Path.home / ".cache" / "crystal"
+                  {% end %}
 
-  CRYSTAL_LIBRARY = begin
-    {% if flag?(:win32) %}
-      Path[::ENV["LOCALAPPDATA"]] / "Programs" / "Crystal"
-    {% else %}
-      Path["usr"] / "lib" / "crystal"
-    {% end %}
-  end
+  CRYSTAL_LIBRARY = {% if flag?(:win32) %}
+                      Path[::ENV["LOCALAPPDATA"], "Programs", "Crystal"]
+                    {% else %}
+                      Path["usr", "lib", "crystal"]
+                    {% end %}
 
   HOST_TARGET = {% if flag?(:win32) %}
                   "windows-x86_64-msvc-unsupported"
