4a381ac10a498f6ae06f29ed4b4ebf8737a7473d
devnote-dev <devnote.dev75@gmail.com>
2023-08-18 23:06:02 +0100

    chore(commands): remove unnecessary marco logic

--- src/commands/setup.cr
+++ src/commands/setup.cr
@@ -7,31 +7,25 @@ module Crimson::Commands
       add_option 'v', "verbose"
     end
 
-    {% if flag?(:win32) %}
-      def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
-        # TODO
+    def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
+      verbose { "checking crimson library path" }
+      unless Dir.exists? ENV::CRIMSON_LIBRARY
+        verbose { "creating crimson library path" }
+        Dir.mkdir_p ENV::CRIMSON_LIBRARY
       end
-    {% else %}
-      def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
-        verbose { "checking crimson library path" }
-        unless Dir.exists? ENV::CRIMSON_LIBRARY
-          verbose { "creating crimson library path" }
-          Dir.mkdir_p ENV::CRIMSON_LIBRARY
-        end
 
-        verbose { "checking crystal cache path" }
-        unless Dir.exists? ENV::CRYSTAL_CACHE
-          verbose { "creating crimson cache path" }
-          Dir.mkdir_p ENV::CRYSTAL_CACHE
-        end
+      verbose { "checking crystal cache path" }
+      unless Dir.exists? ENV::CRYSTAL_CACHE
+        verbose { "creating crimson cache path" }
+        Dir.mkdir_p ENV::CRYSTAL_CACHE
+      end
 
-        if path = Process.find_executable "crystal"
-          unless File.info(path).type.symlink?
-            warn "Crystal appears to be installed without Crimson"
-            warn "Please uninstall it before attempting to install with Crimson"
-          end
+      if path = Process.find_executable "crystal"
+        unless File.info(path).type.symlink?
+          warn "Crystal appears to be installed without Crimson"
+          warn "Please uninstall it before attempting to install with Crimson"
         end
       end
-    {% end %}
+    end
   end
 end
