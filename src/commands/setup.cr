181f8e75e94d72135f2ada791f0efc42e2368339
devnote-dev <devnote.dev75@gmail.com>
2023-08-19 02:03:09 +0100

    feat(commands): add config file check in env

--- src/commands/setup.cr
+++ src/commands/setup.cr
@@ -14,6 +14,17 @@ module Crimson::Commands
         Dir.mkdir_p ENV::LIBRARY
       end
 
+      begin
+        verbose { "checking crimson config file" }
+        _ = Config.load
+      rescue File::NotFoundError
+        verbose { "crimson config not found; creating" }
+        Config.new(nil, nil).save
+      rescue INI::ParseException
+        warn "Config is in an invalid format; overwriting"
+        Config.new(nil, nil).save
+      end
+
       if path = Process.find_executable "crystal"
         unless File.info(path).type.symlink?
           warn "Crystal appears to be installed without Crimson"
