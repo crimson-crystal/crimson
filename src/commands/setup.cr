198547bac113f6b112018dd60f7d8cb50587684d
devnote-dev <devnote.dev75@gmail.com>
2023-08-22 05:57:21 +0100

    feat(commands): methods movement & some cleanup

--- src/commands/setup.cr
+++ src/commands/setup.cr
@@ -31,6 +31,55 @@ module Crimson::Commands
           warn "Please uninstall it before attempting to install with Crimson"
         end
       end
+
+      verbose { "checking executables path" }
+      unless Dir.exists? bin = ENV::LIBRARY / "bin"
+        verbose { "creating executables path" }
+        Dir.mkdir_p bin
+      end
+
+      verbose { "ensuring paths are linked" }
+      if File.symlink? "/usr/local/bin/crystal"
+        unless File.real_path("/usr/local/bin/crystal") == (ENV::LIBRARY / "bin" / "crystal").to_s
+          verbose { "linking executable paths" }
+          link_executables
+        end
+      else
+        verbose { "linking executable paths" }
+        link_executables
+      end
+    end
+
+    private def link_executables : Nil
+      begin
+        File.symlink ENV::LIBRARY / "bin" / "crystal", "/usr/local/bin/crystal"
+      rescue File::Error
+        info "Root permissions are required to link executable"
+        args = ["ln", "-s", (ENV::LIBRARY / "bin" / "crystal").to_s, "/usr/local/bin/crystal"]
+        info "Requested command:"
+        info "sudo #{args.join ' '}".colorize.bold.to_s
+
+        status = Process.run "sudo", args, input: :inherit, output: :inherit, error: :inherit
+        unless status.success?
+          error "Failed to link executable path"
+          error "Please run the command above after installation is complete"
+        end
+      end
+
+      begin
+        File.symlink ENV::LIBRARY / "bin" / "shards", "/usr/local/bin/shards"
+      rescue File::Error
+        info "Root permissions are required to link executable"
+        args = ["ln", "-s", (ENV::LIBRARY / "bin" / "shards").to_s, "/usr/local/bin/shards"]
+        info "Requested command:"
+        info "sudo #{args.join ' '}".colorize.bold.to_s
+
+        status = Process.run "sudo", args, input: :inherit, output: :inherit, error: :inherit
+        unless status.success?
+          error "Failed to link executable path"
+          error "Please run the command above after installation is complete"
+        end
+      end
     end
   end
 end
