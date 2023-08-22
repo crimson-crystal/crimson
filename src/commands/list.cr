c1565b2108969a8821251c0b4735a9a39b278c94
devnote-dev <devnote.dev75@gmail.com>
2023-08-22 04:33:33 +0100

    feat(commands): implement remaining features of list

--- src/commands/list.cr
+++ src/commands/list.cr
@@ -1,5 +1,14 @@
 module Crimson::Commands
   class List < Base
+    private class Result
+      getter version : String
+      property alias : String?
+      property path : String?
+
+      def initialize(@version)
+      end
+    end
+
     def setup : Nil
       @name = "list"
       @summary = "list installed Crystal versions"
@@ -11,39 +20,38 @@ module Crimson::Commands
     end
 
     def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
-      installed = ENV.get_installed_versions
+      installed = ENV.get_installed_versions.sort.reverse
       return if installed.empty?
 
       unless options.has?("alias") || options.has?("path")
-        installed.each { |version| stdout << "• " << version << '\n' }
+        installed.each { |version| stdout << version << '\n' }
         return
       end
 
       config = Config.load
-      aliases = config.aliases.invert
+      _alias = options.has? "alias"
+      path = options.has? "path"
+      results = installed.map { |version| Result.new version }
 
-      stdout << "Version"
-      stdout << " " * (installed.max_of &.size)
-
-      if options.has? "alias"
-        stdout << "Alias"
-        max = config.aliases.keys.max_of &.size
-        stdout << " " * max
-      end
-
-      if options.has? "path"
-        stdout << "Path"
-      end
-
-      stdout << '\n'
-      installed.each do |version|
-        stdout << version << "  "
-
-        if options.has? "alias"
-          config.aliases.fetch(version, "none").ljust stdout, 5
-          stdout << ' '
+      if _alias && config.aliases.any?
+        results.each do |result|
+          if value = config.aliases[result.version]?
+            result.alias = value
+          end
         end
+      end
 
+      if path
+        root = ENV::LIBRARY / "crystal"
+        results.each do |result|
+          result.path = (root / result.version).to_s
+        end
+      end
+
+      results.each do |result|
+        stdout << result.version << "  "
+        stdout << result.alias if _alias
+        stdout << result.path if path
         stdout << '\n'
       end
     rescue File::NotFoundError
