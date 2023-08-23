414ae084c0a3722b8359ea517d345fb5682f6c57
devnote-dev <devnote.dev75@gmail.com>
2023-08-23 02:02:07 +0100

    refactor(commands): rework remove command for aliases & config

--- src/commands/remove.cr
+++ src/commands/remove.cr
@@ -4,20 +4,34 @@ module Crimson::Commands
       @name = "remove"
       @summary = "remove a version of Crystal"
 
-      add_argument "version", required: true
+      add_argument "target", required: true
       add_option 'v', "verbose"
     end
 
+    # NOTE: config is saved preemtively because `at_exit` doesn't work
+    # ref:  https://github.com/crystal-lang/crystal/issues/8687
     def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
-      version = arguments.get("version").as_s
-      path = ENV::LIBRARY / "crystal" / version
+      config = Config.load
+      target = arguments.get("target").as_s
 
-      unless Dir.exists? path
-        error "Crystal version #{version} is not installed"
+      unless target =~ /\d\.\d\.\d/
+        if version = config.aliases.delete target
+          target = version
+        else
+          error "Invalid version format (must be major.minor.patch)"
+          system_exit
+        end
+      end
+
+      config.current = nil if config.current == target
+      config.save
+
+      unless ENV.installed? target
+        error "Crystal version #{target} is not installed"
         system_exit
       end
 
-      FileUtils.rm_rf path
+      FileUtils.rm_rf ENV::LIBRARY / "crystal" / target
     end
   end
 end
