b3e1189f8bac96354f644a55b3310e615431736c
devnote-dev <devnote.dev75@gmail.com>
2023-08-26 01:09:03 +0100

    refactor(commands): redo logic for remove command

--- src/commands/remove.cr
+++ src/commands/remove.cr
@@ -11,28 +11,21 @@ module Crimson::Commands
       add_argument "target", description: "the version or alias version to remove", required: true
     end
 
-    # NOTE: config is saved preemtively because `at_exit` doesn't work
-    # ref:  https://github.com/crystal-lang/crystal/issues/8687
     def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
       config = Config.load
+
       target = arguments.get("target").as_s
-
-      unless target =~ /\d\.\d\.\d/
-        if version = config.aliases.delete target
-          target = version
-        else
-          error "Invalid version format (must be major.minor.patch)"
-          system_exit
-        end
+      if version = config.aliases[target]?
+        target = version
       end
-
-      config.current = nil if config.current == target
-      config.save
 
       unless ENV.installed? target
         error "Crystal version #{target} is not installed"
         system_exit
       end
+
+      config.current = nil if config.current == target
+      config.save
 
       FileUtils.rm_rf ENV::CRYSTAL_PATH / target
     end
