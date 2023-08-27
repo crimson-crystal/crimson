58883b509fad6388fe25e2e3f366072005271ad5
devnote-dev <devnote.dev75@gmail.com>
2023-08-28 00:16:48 +0100

    feat(commands): implement switch period

--- src/commands/switch.cr
+++ src/commands/switch.cr
@@ -7,10 +7,12 @@ module Crimson::Commands
         Manages the available Crystal version on the system. By default this command
         will print the current set Crystal version. Specifying a version or version
         alias will set that as the available version on the system (meaning it will
-        switch the Crystal and Shards executables).
+        switch the Crystal and Shards executables). Specifying a period "." will
+        swap the current version with the default version then switch to it.
         DESC
 
       add_usage "switch"
+      add_usage "switch ."
       add_usage "switch [-v|--verbose] <target>"
 
       add_argument "target", description: "the version or version alias to switch to"
@@ -25,6 +27,19 @@ module Crimson::Commands
       end
 
       target = arguments.get("target").as_s
+      if target == "."
+        unless default = config.default
+          error "No default version set"
+          system_exit
+        end
+
+        Internal.switch ENV::LIBRARY_CRYSTAL / default
+        config.default = config.current
+        config.current = default
+        config.save
+        return
+      end
+
       if version = config.aliases[target]?
         target = version
       end
