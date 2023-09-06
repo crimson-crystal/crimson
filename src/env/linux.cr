ca7792f4c10119b1ba93fe0341e30fedafe772d1
devnote-dev <devnote.dev75@gmail.com>
2023-09-06 02:19:52 +0100

    refactor(env): implement support for other distros

--- src/env/linux.cr
+++ src/env/linux.cr
@@ -81,14 +81,29 @@ module Crimson::ENV
   end
 
   def self.install_dependencies(prompt : Bool) : Nil
-    args = {"apt-get", "install", "-y", "gcc", "pkg-config", "libpcre3-dev", "libpcre2-dev", "libevent-dev"}
+    pkg_cmd = find_package_manager
+    unless pkg_cmd
+      error "Could not locate the system package manager"
+      error "Crystal dependencies must be installed manually"
+      return
+    end
+
+    deps = get_system_dependencies pkg_cmd[0]
+    unless deps
+      error "Could not resolve Crystal dependencies for this distribution"
+      error "This distribution may require setting up an RPM signing key"
+      error "Which cannot be done using Crimson"
+      return
+    end
+
     if prompt
       puts "Crystal requires the following dependencies to compile:"
-      puts args[3..].join(' ').colorize.bold
+      puts deps.join(' ').colorize.bold
       return unless should_continue?
       puts
     end
 
+    args = pkg_cmd + deps
     puts "Running command:"
     puts "sudo #{args.join ' '}".colorize.bold
     puts
@@ -102,15 +117,30 @@ module Crimson::ENV
   end
 
   def self.install_additional_dependencies(prompt : Bool) : Nil
-    args = {"apt-get", "install", "-y", "libssl-dev", "libz-dev", "libxml2-dev", "libgmp-dev", "libyaml-dev"}
+    pkg_cmd = find_package_manager
+    unless pkg_cmd
+      error "Could not locate the system package manager"
+      error "Crystal dependencies must be installed manually"
+      return
+    end
+
+    deps = get_system_additional_dependencies pkg_cmd[0]
+    unless deps
+      error "Could not resolve Crystal dependencies for this distribution"
+      error "This distribution may require setting up an RPM signing key"
+      error "Which cannot be done using Crimson"
+      return
+    end
+
     if prompt
       puts "Crystal uses some additional dependencies for compilation"
       puts "These dependencies are not required but may improve compilation:"
-      puts args[3..].join(' ').colorize.bold
+      puts deps.join(' ').colorize.bold
       return unless should_continue?
       puts
     end
 
+    args = pkg_cmd + deps
     puts "Running command:"
     puts "sudo #{args.join ' '}".colorize.bold
     puts
@@ -121,5 +151,58 @@ module Crimson::ENV
     return if status.success?
     error "Please run the command below after the process is complete:"
     puts args.join(' ').colorize.bold
+  end
+
+  private def self.find_package_manager : Array(String)?
+    info = if File.exists?("/etc/os-release")
+             INI.parse File.read "/etc/os-release"
+           elsif File.exists?("/usr/lib/os-release")
+             INI.parse File.read "/usr/lib/os-release"
+           else
+             return nil
+           end
+    return nil unless id_like = info[""]["ID_LIKE"]?
+
+    case distro = id_like.strip '"'
+    when "ubuntu", "debian", "ubuntu debian"
+      %w[apt-get install -y]
+    when "alpine"
+      %w[apk add]
+    when "centos", "rhel"
+      %w[yum install -y]
+    when "fedora"
+      %w[dnf install -y]
+    when "arch"
+      %w[pacman -S]
+    end
+  end
+
+  private def self.get_system_dependencies(tool : String) : Array(String)?
+    case tool
+    when "apt-get"
+      %w[gcc pkg-config libpcre3-dev libpcre2-dev libevent-dev]
+    when "apk"
+      %w[
+        gc gc-dev gcc libgcc
+        libatomic_ops llvm16-libs
+        libevent libevent-dev libevent-static
+        libstdc++ musl musl-dev pcre2 pcre2-dev
+      ]
+      # when "yum", "dnf"
+    when "pacman"
+      %w[gc libevent llvm-libs pcre2]
+    end
+  end
+
+  private def self.get_system_additional_dependencies(tool : String) : Array(String)?
+    case tool
+    when "apt-get"
+      %w[libssl-dev libz-dev libxml2-dev libgmp-dev libyaml-dev]
+    when "apk"
+      %w[gmp-dev]
+      # when "yum", "dnf"
+    when "pacman"
+      %w[gmp libxml2 libyaml llvm git inetutils]
+    end
   end
 end
