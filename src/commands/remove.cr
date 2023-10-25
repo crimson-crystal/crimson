ee9203f1b1ce64c5cad90e12f5959515d3ed1762
devnote-dev <devnote.dev75@gmail.com>
2023-10-25 18:19:12 +0100

    feat(commands): support removing multiple targets

--- src/commands/remove.cr
+++ src/commands/remove.cr
@@ -3,31 +3,37 @@ module Crimson::Commands
     def setup : Nil
       @name = "remove"
       @summary = "remove a version of Crystal"
-      @description = "Removes an installed version of Crystal."
+      @description = "Removes one or more installed versions of Crystal."
 
       add_alias "rm"
-      add_usage "remove <target>"
+      add_usage "remove <targets...>"
 
-      add_argument "target", description: "the version or alias version to remove", required: true
+      add_argument "targets",
+        description: "the versions or aliased versions to remove",
+        multiple: true,
+        required: true
     end
 
     def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
       config = Config.load
 
-      target = arguments.get("target").as_s
-      if version = config.aliases.delete target
-        target = version
+      targets = arguments.get("target").as_a
+      targets.each do |target|
+        if version = config.aliases.delete target
+          target = version
+        end
+
+        unless ENV.installed? target
+          error "Crystal version #{target} is not installed"
+          next
+        end
+
+        config.current = config.default if config.current == target
+
+        FileUtils.rm_rf ENV::LIBRARY_CRYSTAL / target
       end
 
-      unless ENV.installed? target
-        error "Crystal version #{target} is not installed"
-        system_exit
-      end
-
-      config.current = config.default if config.current == target
       config.save
-
-      FileUtils.rm_rf ENV::LIBRARY_CRYSTAL / target
     end
   end
 end
