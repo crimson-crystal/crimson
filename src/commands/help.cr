b43509e0b4a44b052359191065825739684efba2
devnote-dev <devnote.dev75@gmail.com>
2024-09-30 02:19:08 +0100

    fix(commands): skip help option check in help command

--- src/commands/help.cr
+++ src/commands/help.cr
@@ -7,6 +7,10 @@ module Crimson::Commands
       add_argument "command"
     end
 
+    def pre_run(arguments : Cling::Arguments, options : Cling::Options) : Nil
+      Colorize.enabled = false if options.has? "no-color"
+    end
+
     def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
       main = parent.as(Cling::Command)
       command = arguments.get?("command").try &.as_s
