f01dde5b4e7c81f58d80adbf7ac1199fc46e8fc8
devnote-dev <devnote.dev75@gmail.com>
2024-10-02 17:09:21 +0100

    chore: remove platform scripts, add shard debug script

--- scripts/nightly.sh
+++
@@ -1,9 +0,0 @@
-crystal build src/main.cr -o crimson
-sha256sum crimson > checksums.txt
-tar -zcf crimson-nightly-linux-x86_64.tar.gz crimson checksums.txt
-
-# gh release delete nightly -y || true
-gh release create nightly -dpt nightly
-gh release upload nightly crimson-nightly-linux-x86_64.tar.gz
-
-rm crimson checksums.txt crimson-nightly-linux-x86_64.tar.gz
